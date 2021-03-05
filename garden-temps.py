f85f4f05bf545b7b3fb71adaeb7549b4b6dda9ac
Wade Dorrell <wade@dorrells.org>
2021-03-04 23:13:02 -0700

    Update garden-temps.py

--- garden-temps.py
+++ garden-temps.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import time
 import paho.mqtt.client as mqtt
@@ -6,31 +7,29 @@ def read_temp(device_path):
     file = open(device_path, 'r')
     data = file.readlines()
     file.close()
-    return float(data[1].split("t=")[1]) / 1000.0 * 9/5 + 32
+    return round(float(data[1].split("t=")[1]) / 1000.0 * 9/5 + 32, 2)
 
 def main():
-    MQTT_USER = os.getenv('MQTT_USER')
-    MQTT_PWD = os.getenv('MQTT_PWD')
-    MQTT_BROKER = os.getenv('MQTT_BROKER') # server name
+    MQTT_BROKER = os.getenv('MQTT_BROKER') # e.g. hostname/IP
     DEVICE_PATH = os.getenv('DEVICE_PATH') # e.g. /sys/bus/w1/devices/
     INTERVAL_S = os.getenv('INTERVAL_S') # seconds
 
     print(f'garden-temps sending temp data to {MQTT_BROKER}')
 
     client = mqtt.Client()
-    # client.username_pw_set(MQTT_USER, MQTT_PWD)
     client.connect(MQTT_BROKER)
     client.loop_start()
     while True:
         print('tick')
+        time = datetime.datetime.now()
 
         for file in os.listdir(DEVICE_PATH):
             device_path = os.path.join(DEVICE_PATH, file)
             device_path = os.path.join(device_path, 'w1_slave')
-            if (os.path.isfile(device_path)):                
+            if (os.path.isfile(device_path)):
                 temp = read_temp(device_path)
-                event_name = file
-                client.publish('temperature', f'{file},{temp}')
+                event_name = file            
+                client.publish('temperature', f'{time},{file},{temp}')
 
         time.sleep(int(INTERVAL_S))
 
