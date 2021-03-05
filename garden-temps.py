c63ca264379a3e17ad9379bce0fc564e85e9dc17
Wade Dorrell <wade@dorrells.org>
2021-03-04 22:28:36 -0700

    Update garden-temps.py

--- garden-temps.py
+++ garden-temps.py
@@ -25,11 +25,14 @@ def main():
         print('tick')
 
         for file in os.listdir(DEVICE_PATH):
+            print(file)
             device_path = os.path.join(DEVICE_PATH, file)
+            print(device_path)
             device_path = os.path.join(device_path, 'w1_slave')
+            print(device_path)
             temp = read_temp(device_path)
             event_name = file
-            client.publish(event_name, temp)
+            client.publish('temperature', f'{file},{temp}')
 
         time.sleep(int(INTERVAL_S))
 
