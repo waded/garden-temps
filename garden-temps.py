8e1ede4b10c77479b3b4c7881dd8832b245e19ac
Wade Dorrell <wade@dorrells.org>
2021-03-06 18:17:46 -0700

    Clean up

--- garden-temps.py
+++ garden-temps.py
@@ -3,33 +3,44 @@ import os
 import time
 import paho.mqtt.client as mqtt
 
-def read_temp(device_path):
-    file = open(device_path, 'r')
-    data = file.readlines()
-    file.close()
-    return round(float(data[1].split("t=")[1]) / 1000.0 * 9.0/5.0 + 32, 1)
+def read_temp(device_file):
+    with open(device_file, 'r') as f:
+        data = f.readlines()
+
+    temp_c = float(data[1].split("t=")[1]) / 1000.0
+    temp_f = temp_c * 9.0/5.0 + 32
+    return round(temp_f, 1)
 
 def main():
-    MQTT_BROKER = os.getenv('MQTT_BROKER') # e.g. hostname/IP
-    DEVICE_PATH = os.getenv('DEVICE_PATH') # e.g. /sys/bus/w1/devices/
-    INTERVAL_S = os.getenv('INTERVAL_S') # seconds
+     # e.g. hostname or IP
+    MQTT_BROKER = os.getenv('MQTT_BROKER')
 
-    print(f'garden-temps sending temp data to {MQTT_BROKER}')
+    # typ. /sys/bus/w1/devices/
+    DEVICE_PATH = os.getenv('DEVICE_PATH')
+
+    # in seconds
+    INTERVAL_S = os.getenv('INTERVAL_S')
+
+    print(f'connecting to {MQTT_BROKER}')
 
     client = mqtt.Client()
     client.connect(MQTT_BROKER)
     client.loop_start()
+
+    print(f'sending data to {MQTT_BROKER}')
+
     while True:
-        print('tick')
         now = datetime.now().isoformat(timespec='seconds')
 
-        for file in os.listdir(DEVICE_PATH):
-            device_path = os.path.join(DEVICE_PATH, file)
-            device_path = os.path.join(device_path, 'w1_slave')
-            if (os.path.isfile(device_path)):
-                temp = read_temp(device_path)
-                event_name = file            
-                client.publish('temperature', f'{now},{file},{temp}')
+        print(f'tick:{now}')
+
+        for f in os.listdir(DEVICE_PATH):
+            device_file = os.path.join(DEVICE_PATH, f, 'w1_slave')
+            if (os.path.isfile(device_file)):
+                temp = read_temp(device_file)
+                event_name = f'temperature/{f}'
+                message = f'{f},{now},{temp}'
+                client.publish(event_name, message)
 
         time.sleep(int(INTERVAL_S))
 
